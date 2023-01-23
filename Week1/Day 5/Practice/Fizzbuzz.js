965914349d76a62f1490ab90f1c00ac860c069ad
Soumaya Cherichi <soumayacherichi@gmail.com>
2023-01-23 12:30:03 +0100

    Fizzbuzz

---
+++ Week1/Day 5/Practice/Fizzbuzz.js
@@ -0,0 +1,18 @@
+for(vari=1;i<101;i++)
+{
+  if((i%3)=0){
+    console.log(i="Fizz");
+    }
+  if((i%5)=0){
+    console.log(i="Buzz");
+}
+  if(((i%3)=0)&&((i%5)=0)){console.log(i="FizzBuzz");
+}
+}
+console.log(i);
+
+
+
+
+
+
